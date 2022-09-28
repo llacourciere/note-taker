071628aed97d7673fb6cb48c60b7c47e48558b82
LaLacou <108777796+users.noreply.github.com>
2022-09-27 20:30:00 -0700

    added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ router.get('/notes', (req,res)=> res.json(store));
 //this enables the save button to work using the post method
 router.post('/notes', ({body}, res)=> {
     body.id = uuidv4();
-
+//storing the note in the db.json file and catching for an error if one occurs
     store.push(body);
     writeFile('./db/db.json', JSON.stringify(store), (err)=>{
         if(err) throw err;
@@ -21,8 +21,11 @@ router.post('/notes', ({body}, res)=> {
 });
 //this deletes a note using its unique id
 router.delete('/notes/:id', (req, res)=> {
+    //reading notes from db.json
     let db = JSON.parse(fs.readFileSync('./db/db.json'))
+    //remove the note with the id being targeted
     let deleteNotes = db.filter(item => item.id !== req.params.id);
+    //rewriting notes to json
     fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
     res.json(deleteNotes);
 
