1655f33c8386cfbc1b7d73ed74b71371c845475d
LaLacou <108777796+users.noreply.github.com>
2022-09-27 20:05:53 -0700

    added gitignore

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+//calling express to this document
 const express = require('express');
 
 const app = express();
+//creating the port
 const PORT = process.env.PORT || 3001;
-
+//adding middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+//adding files for the server to use
 app.use(require("./routes"));
 app.use(express.static('public'));
 
